78001133382878f8f7d549b4cb3b96622bb4d2a2
leacappello <81253331+leacappello@users.noreply.github.com>
2026-07-21 10:28:17 +0200

    US 9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -7,20 +7,26 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use RuntimeException;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Image;
+use Spatie\Image\Enums\AlignPosition;
 
 class ResizeImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $w;
-    private $h;
-    private $fileName;
-    private $path;
+    private int $w;
+    private int $h;
+    private string $fileName;
+    private string $path;
 
-    public function __construct($filePath, $w, $h)
-    {
+    public function __construct(
+        string $filePath,
+        int $w,
+        int $h
+    ) {
         $this->path = dirname($filePath);
         $this->fileName = basename($filePath);
         $this->w = $w;
@@ -29,23 +35,50 @@ class ResizeImage implements ShouldQueue
 
     public function handle(): void
     {
-        $w = $this->w;
-        $h = $this->h;
-
-        $srcPath = storage_path()
-            . '/app/public/'
+        $srcPath = storage_path(
+            'app/public/'
             . $this->path
             . '/'
-            . $this->fileName;
+            . $this->fileName
+        );
 
-        $destPath = storage_path()
-            . '/app/public/'
+        $destPath = storage_path(
+            'app/public/'
             . $this->path
-            . "/crop_{$w}x{$h}_"
-            . $this->fileName;
+            . "/crop_{$this->w}x{$this->h}_"
+            . $this->fileName
+        );
+
+        $watermarkPath = resource_path(
+            'img/watermark.png'
+        );
+
+        if (!file_exists($srcPath)) {
+            throw new RuntimeException(
+                "Immagine sorgente non trovata: {$srcPath}"
+            );
+        }
+
+        if (!file_exists($watermarkPath)) {
+            throw new RuntimeException(
+                "Watermark non trovato: {$watermarkPath}"
+            );
+        }
 
         Image::load($srcPath)
-            ->crop($w, $h, CropPosition::Center)
+            ->crop(
+                $this->w,
+                $this->h,
+                CropPosition::Center
+            )
+         ->watermark(
+             base_path('resources/img/watermark.png'),
+             width: 70,
+             height: 70,
+             paddingX: 10,
+             paddingY: 10,
+             paddingUnit: Unit::Pixel
+            )
             ->save($destPath);
     }
 }
